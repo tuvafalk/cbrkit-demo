cd6dfce5c330d0f59a8991a50f9198fce05cc04f
Mirko Lenz <mirko@mirkolenz.com>
2024-03-08 10:34:21 +0100

    fix: update default similarity

--- cbrkit_demo/retrieval.py
+++ cbrkit_demo/retrieval.py
@@ -8,18 +8,13 @@ query: Car = {}
 
 global_similarity = cbrkit.sim.attribute_value(
     attributes={
-        "year": cbrkit.sim.numbers.linear(max=100),
-        "paint_color": cbrkit.sim.strings.taxonomy.load(
-            "data/taxonomies/paint_color.yaml",
-            measure=cbrkit.sim.strings.taxonomy.user_weights("optimistic"),
-        ),
         "manufacturer": cbrkit.sim.strings.taxonomy.load(
             "data/taxonomies/manufacturer.yaml",
             measure=cbrkit.sim.strings.taxonomy.path_steps(),
         ),
     },
     types={
-        str: cbrkit.sim.strings.levenshtein(),
+        str: cbrkit.sim.strings.levenshtein(case_sensitive=False),
         int: cbrkit.sim.numbers.linear(max=9999999),
     },
     aggregator=cbrkit.sim.aggregator("mean"),
